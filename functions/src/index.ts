1c51158d9ca6fc7e3bbe351c5b514b9ba9377d95
Luis Eduardo <luiseperezp.10@gmail.com>
2021-04-11 23:51:03 -0500

    feat(app): add functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,47 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require('./servicesAccount.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://reto-oechsle-d0c94.firebaseio.com',
+});
+
+const db = admin.firestore();
+
+const cors = require('cors')({
+  origin: true,
+}); 
+
+exports.getClients = functions.https.onRequest((req, res)=>{
+  try {
+    cors(req, res, async () => {
+      const clientsRef = db.collection('clients');
+        const docsSnap = await clientsRef.get();
+    const listClients = docsSnap.docs.map((doc) => doc.data());
+    res.json(listClients)
+    })
+  } catch (error) {
+    res.json({error: `${error}`})
+  }
+});
+
+exports.addClients = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+      try {
+      const clientsRef = db.collection('clients');
+      const docsSnap = await clientsRef.get();
+      const listClients = docsSnap.docs.map((doc) => doc.data());
+      const size = listClients.length + 1;
+      const { name,lastname,dateBirth,age } = req.body;
+      const id = (size).toString().padStart(3, '0')
+      const data = { id, name,lastname,dateBirth,age}
+      await clientsRef.doc(`${id}`).set(data);
+      res.json({ message: `Se ha registrado correctamente.` });
+    } catch (error) {
+      res.json({error: `${error}`})
+    }
+    })
+});
